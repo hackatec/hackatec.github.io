4029428f21c1f50be62486196a9a49327de2ae89
Ayelen Guerra <Ayelen Guerra>
2016-05-24 01:26:13 -0300

    modificaciones varias

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    var showChar = 200;
+    var ellipsestext = "...";
+    var moretext = "(ver más)";
+    var lesstext = "(ver menos)";
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+    console.log(h);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+            // var html = c;
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
